c58f60b2c522841a18aa001abcb75b43b96ae91f
shreyasvasuki <shreyas.vasuki@gmail.com>
2021-04-13 02:07:27 +0530

    Readme

---
+++ mini project/welcome.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+  char ch;
+  scanf("%s",ch);
+  printf ("        **WECOME TO SNAKES AND LADDERS**        \n");
+  printf ("Instructions\n");
+  printf ("-> Snakes:- 25 to 9,\t 65 to 40,\t 99 to  1.\n");
+  printf ("-> Ladders:- 13 to 42,\t 60 to 83,\t 70 to 93.\n");
+  printf ("choose player1 or player2:type 1,2 or 3 to select the options\n");
+  printf ("1. Player 1\n");
+  printf ("2. Player 2\n");
+  printf ("3. exit\n");
+  scanf()
+  
+}
