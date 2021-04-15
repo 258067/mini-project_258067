9f90197c5f97a8a0db0f1e164c074d604f4176e5
258067 <81506807+258067@users.noreply.github.com>
2021-04-15 20:28:47 +0530

    Add files via upload

---
+++ 3_Implementation/inc/dice.h
@@ -0,0 +1,25 @@
+/**
+ * @file dice.h
+ * @author Shreyus R D (you@domain.com)
+ * @brief Header file for dice_output function
+ * @version 0.1
+ * @date 2021-04-14
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+#ifndef _DICE_H_
+#define _DICE_H_
+/**
+ * @brief Function to find the output of a dice roll
+ * @return int result of dice_output operation
+ * 
+ */
+int dice_output();
+/**
+ * @brief Function to check if output of dice is 6
+ * @param[in] diceout Number whose value is to be checked for 6
+ * @return int result after checking
+ */
+char *diceout6();
+#endif/*_DICE_H_*/
