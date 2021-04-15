07a86c427dad0600b6aa34d576386b3853047f38
Bhargav R <40035575+bgvmysore@users.noreply.github.com>
2021-04-15 21:39:02 +0530

    Update dice.c

--- 3_Implementation/src/dice.c
+++ 3_Implementation/src/dice.c
@@ -4,7 +4,13 @@
 int dice_output()
 {
     int upper = 6, lower = 1, dice;
+    
+    #ifdef VALGRIND
+    srand (0);
+    #else
     srand (time (0));
+    #endif
+    
     A:dice = (rand () % (upper - lower + 1)) + lower;
     if (dice == 0)
       goto A;
